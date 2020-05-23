fe4c1cf35b37faa71de2532972d75af6ee2808cb
choo121600 <choo121600@gmail.com>
2020-05-24 05:56:23 +0900

    test heroku

--- server/index.js
+++ server/index.js
@@ -86,6 +86,10 @@ app.get("/api/user/logout", auth, (req, res) => {
   );
 });
 
+app.get("/", (req, res) => {
+  res.json({ hello: "helloooo" });
+});
+
 app.listen(port, () => {
   console.log(`server running port ${port}`);
 });
